e894f3f28cc23c42b52c50be586fc3b80b309709
Cynthia <cycylins@hotmail.com>
2024-12-21 11:08:03 -0300

    Implementação do score

--- Jogo_Detona_Ralph_com_JavaScript/assets/scripts/engine.js
+++ Jogo_Detona_Ralph_com_JavaScript/assets/scripts/engine.js
@@ -3,35 +3,51 @@ const state = {
         squares: document.querySelectorAll(".square"),
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#time-left"),
-        score: document.querySelector("#score")
+        score: document.querySelector("#score"),
     },
-    
+
     values: {
         timeId: null,
-        gameVelocity: 1000
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
     }    
 };
 
+// Determina que o inimigo irá aparecer em cada quadrado de maneira randomica//
 function randomSquare() {
     state.view.squares.forEach((square) =>{
         square.classList.remove("enemy");
     });
 
-    let randomNumber = Math.floor(Math.random()* 9);
+    let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
 
+// Determina a velocidade com que o inimigo mudará de quadrado//
 function moveEnemy() {
     state.values.timeId = setInterval(randomSquare, state.values.gameVelocity);
 }
 
+//Determina o que deverá ser feito ao clicar em cima do quadrado contendo o inimigo//
 function addListenerHitbox() {
-    state.view.forEach((square) => {})
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            };
+        });
+    });
 }
 
+// Determina como o jogo deve se comportar ao ser inicializado//
 function initialize() {
-    randomSquare();
+    moveEnemy();
+    addListenerHitbox();
 }
 
 initialize();
