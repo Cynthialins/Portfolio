8965b1bd495406cef7f99500595813a0cc3fb117
Cynthia <cycylins@hotmail.com>
2025-04-30 12:39:02 -0300

    Adicionando o controle de teclas (toggle)

--- Simulador_Piano_JavaScript-e-CSS/src/scripts/engine.js
+++ Simulador_Piano_JavaScript-e-CSS/src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
 let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
@@ -16,7 +17,6 @@ const playTune = (key) => {
 };
 
 pianoKeys.forEach((key) => {
-    console.log(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key));
     mapedKeys.push(key.dataset.key);
 });
@@ -29,6 +29,12 @@ document.addEventListener("keydown", (e) => {
 
 const handleVolume = (e) => {
     audio.volume = e.target.value;
-}
+  };
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+};
 
 volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
